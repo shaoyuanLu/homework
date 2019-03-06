a20dc5d7a77b4dd769bee06d947bc503ebfcceb6
shaoyuanLu <shaoyuan812@gmail.com>
2019-03-06 16:08:12 +0800

    更新作業2

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Tester {
 
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
         int tickets;
         do {
-            Scanner scanner=new Scanner(System.in);
             System.out.println("Please enter number of tickets: ");
             tickets=scanner.nextInt();
             if (tickets!=-1) {
